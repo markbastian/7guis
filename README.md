6c98b1f1546889f447b4ff65dc635a7421eeeef2
Mark Bastian <markbastian@gmail.com>
2020-12-11 10:02:24 -0700

    cell formatting

--- README.md
+++ README.md
@@ -19,7 +19,6 @@ The Cells API is fairly cool. I broke it out as two cljc nses so it could potent
   * Self-hosted cljs for cells vs. a parser?
   * More functions?
   * Evaluate watch-per-cell vs. global watch for performance
-  * Format column header cells and row index cells to gray and center aligned
 * Testing
   * Move rich comment blocks into tests.
 
@@ -34,3 +33,8 @@ The Cells API is fairly cool. I broke it out as two cljc nses so it could potent
   * Execute `(shadow.cljs.devtools.api/nrepl-select :frontend)` in your ns.
 * `clj -A:shadow-cljs release frontend` will create a minified js file.
 * Refresher on [gh-pages](https://jiafulow.github.io/blog/2020/07/09/create-gh-pages-branch-in-existing-repo/) creation.
+
+## Links
+These can be useful for finding events, css, etc.
+
+* [HTML Cell Propeties & Events](http://help.openspan.com/80/HTML_Table_Designer/html_table_designer_-_Cell_-_properties,_methods_and_events.htm)
--- src/main/guis/gui07.cljs
+++ src/main/guis/gui07.cljs
@@ -6,79 +6,83 @@
                (xcell/update-cell! :A1 "42")
                (xcell/update-cell! :B2 "=(+ A1 1)")))
 
+(defn enter-handler [e cell]
+  (let [v (.-value (.-target e))]
+    (when (= (.-key e) "Enter")
+      (doto state
+        (xcell/update-cell! cell v)
+        (swap! dissoc :temp-value)))))
+
+(defn render-table-header [border-style ncols]
+  [:thead
+   [:tr (assoc-in border-style [:style :background-color] :gray)
+    (doall
+      (for [i (range (inc ncols)) :let [col (char (+ 64 i))]]
+        ^{:key (str "col:" col)}
+        [:th (assoc-in border-style [:style :text-align] :center)
+         (when-not (zero? i) col)]))]])
+
+(defn render-table-body [state nrows ncols]
+  [:tbody
+   (doall
+     (for [row-index (map inc (range nrows))]
+       ^{:key (str "data col" row-index)}
+       [:tr
+        (doall
+          (for [column-index (range (inc ncols))
+                :let [col (char (+ 64 column-index))
+                      cell-index (str col row-index)
+                      cell (keyword cell-index)]]
+            ^{:key (str "cell:" cell-index)}
+            [:td (cond-> {:style {:border "1px solid black"}}
+                         (zero? column-index)
+                         (-> (assoc-in [:style :text-align] :center)
+                             (assoc-in [:style :background-color] :gray)))
+             (if (zero? column-index)
+               row-index
+               [:input.form-control
+                {:type       "text"
+                 :style      {:border :none :width :120px}
+                 :value      (let [{:keys [temp-value active-cell] :as s} @state]
+                               (or
+                                 (and (= cell active-cell) temp-value)
+                                 (get-in s [cell :value])))
+                 :on-click   (fn [_]
+                               (let [{:keys [active-cell temp-value]} @state]
+                                 (when
+                                   (and active-cell temp-value)
+                                   (xcell/update-cell! state active-cell temp-value))
+                                 (doto state
+                                   (swap! assoc
+                                          :active-cell cell
+                                          :temp-value (get-in @state [cell :text])))))
+                 :on-change  (fn [e]
+                               (let [v (.-value (.-target e))]
+                                 (swap! state assoc :temp-value v)))
+                 :onKeyPress (fn [e] (enter-handler e cell))}])]))]))])
+
 (defn render []
   (let []
     (fn []
       (let [nrows 100
             ncols 26
-            cell-style {:style {:border "1px solid black"}}]
+            border-style {:style {:border "1px solid black"}}]
         [:div
          [:h2 "Task 7: Cells"]
          [:div {:style {:width :100% :height :600px :overflow-x :auto}}
-          [:table cell-style
-           [:thead
-            [:tr cell-style
-             (doall
-               (for [i (range (inc ncols))
-                     :let [col (char (+ 64 i))]]
-                 ^{:key (str "col:" col)}
-                 [:th cell-style (when-not (zero? i) col)]))]]
-           [:tbody
-            (doall
-              (for [row-index (map inc (range nrows))]
-                ^{:key (str "data col" row-index)}
-                [:tr
-                 (doall
-                   (for [column-index (range (inc ncols))
-                         :let [col (char (+ 64 column-index))
-                               cell-index (str col row-index)
-                               cell (keyword cell-index)]]
-                     ^{:key (str "cell:" cell-index)}
-                     ;http://help.openspan.com/80/HTML_Table_Designer/html_table_designer_-_Cell_-_properties,_methods_and_events.htm
-                     [:td cell-style
-                      (if (zero? column-index)
-                        row-index
-                        [:input.form-control
-                         {:type       "text"
-                          :style      {:border :none :width :120px}
-                          :value      (let [{:keys [temp-value active-cell] :as s} @state]
-                                        (or
-                                          (and (= cell active-cell) temp-value)
-                                          (get-in s [cell :value])))
-                          :on-click   (fn [_]
-                                        (let [{:keys [active-cell temp-value]} @state]
-                                          (when
-                                            (and active-cell temp-value)
-                                            (xcell/update-cell! state active-cell temp-value))
-                                          (doto state
-                                            (swap! assoc
-                                                   :active-cell cell
-                                                   :temp-value (get-in @state [cell :text])))))
-                          :on-change  (fn [e]
-                                        (let [v (.-value (.-target e))]
-                                          (swap! state assoc :temp-value v)))
-                          :onKeyPress (fn [e]
-                                        (let [v (.-value (.-target e))]
-                                          (when (= (.-key e) "Enter")
-                                            (doto state
-                                              (xcell/update-cell! cell v)
-                                              (swap! dissoc :temp-value)))))}])]))]))]]]
+          [:table border-style
+           [render-table-header border-style ncols]
+           [render-table-body state nrows ncols]]]
          (when-some [active-cell (:active-cell @state)]
            [:div.input-group-prepend
             [:span#basic-addon1.input-group-text (str (name active-cell) ":")]
             [:input.form-control
              {:type       "text"
-              :value      (let [{:keys [temp-value]} @state]
-                            temp-value)
+              :value      (:temp-value @state)
               :on-change  (fn [e]
                             (let [v (.-value (.-target e))]
                               (swap! state assoc :temp-value v)))
-              :onKeyPress (fn [e]
-                            (let [v (.-value (.-target e))]
-                              (when (= (.-key e) "Enter")
-                                (doto state
-                                  (xcell/update-cell! active-cell v)
-                                  (swap! dissoc :temp-value)))))}]])
+              :onKeyPress (fn [e] (enter-handler e active-cell))}]])
          [:h5 "About"]
          [:p "This is an Excel-like spreadsheet with formulas, propagation, etc. Here are some things to try:"]
          [:ul
