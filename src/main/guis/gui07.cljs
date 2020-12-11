1768456c8a919cfecfee288534ff8a03f6607277
Mark Bastian <markbastian@gmail.com>
2020-12-11 13:37:42 -0700

    Breaking up cell rendering into subfunctions. Consider using cursors for cells.

--- src/main/guis/gui07.cljs
+++ src/main/guis/gui07.cljs
@@ -2,11 +2,14 @@
   (:require [reagent.core :as r]
             [xcell.core :as xcell]))
 
-(def state (-> (r/atom {})
-               (xcell/update-cell! :A1 "42")
-               (xcell/update-cell! :B2 "=(+ A1 1)")))
+;This is a convenience for debugging. It allows you to inspect the state that
+;normally would be embedded in the component.
+(def global-state
+  (-> (r/atom {})
+      (xcell/update-cell! :A1 "42")
+      (xcell/update-cell! :B2 "=(+ A1 1)")))
 
-(defn enter-handler [e cell]
+(defn press-enter-handler [e state cell]
   (let [v (.-value (.-target e))]
     (when (= (.-key e) "Enter")
       (doto state
@@ -22,6 +25,38 @@
         [:th (assoc-in border-style [:style :text-align] :center)
          (when-not (zero? i) col)]))]])
 
+(defn click-cell-handler [state cell]
+  (let [{:keys [active-cell temp-value]} @state]
+    (when
+      (and active-cell temp-value)
+      (xcell/update-cell! state active-cell temp-value))
+    (doto state
+      (swap! assoc
+             :active-cell cell
+             :temp-value (get-in @state [cell :text])))))
+
+(defn cell-change-handler [event state]
+  (let [v (.-value (.-target event))]
+    (swap! state assoc :temp-value v)))
+
+(defn render-cell [state cell column-index row-index]
+  [:td (cond-> {:style {:border "1px solid black"}}
+               (zero? column-index)
+               (-> (assoc-in [:style :text-align] :center)
+                   (assoc-in [:style :background-color] :gray)))
+   (if (zero? column-index)
+     row-index
+     [:input.form-control
+      {:type       "text"
+       :style      {:border :none :width :120px}
+       :value      (let [{:keys [temp-value active-cell] :as s} @state]
+                     (or
+                       (and (= cell active-cell) temp-value)
+                       (get-in s [cell :value])))
+       :on-click   (fn [_] (click-cell-handler state cell))
+       :on-change  (fn [e] (cell-change-handler e state))
+       :onKeyPress (fn [e] (press-enter-handler state e cell))}])])
+
 (defn render-table-body [state nrows ncols]
   [:tbody
    (doall
@@ -34,35 +69,11 @@
                       cell-index (str col row-index)
                       cell (keyword cell-index)]]
             ^{:key (str "cell:" cell-index)}
-            [:td (cond-> {:style {:border "1px solid black"}}
-                         (zero? column-index)
-                         (-> (assoc-in [:style :text-align] :center)
-                             (assoc-in [:style :background-color] :gray)))
-             (if (zero? column-index)
-               row-index
-               [:input.form-control
-                {:type       "text"
-                 :style      {:border :none :width :120px}
-                 :value      (let [{:keys [temp-value active-cell] :as s} @state]
-                               (or
-                                 (and (= cell active-cell) temp-value)
-                                 (get-in s [cell :value])))
-                 :on-click   (fn [_]
-                               (let [{:keys [active-cell temp-value]} @state]
-                                 (when
-                                   (and active-cell temp-value)
-                                   (xcell/update-cell! state active-cell temp-value))
-                                 (doto state
-                                   (swap! assoc
-                                          :active-cell cell
-                                          :temp-value (get-in @state [cell :text])))))
-                 :on-change  (fn [e]
-                               (let [v (.-value (.-target e))]
-                                 (swap! state assoc :temp-value v)))
-                 :onKeyPress (fn [e] (enter-handler e cell))}])]))]))])
+            [render-cell state cell column-index row-index]))]))])
 
 (defn render []
-  (let []
+  ;The state could simply be initialized here.
+  (let [state global-state]
     (fn []
       (let [nrows 100
             ncols 26
@@ -82,7 +93,7 @@
               :on-change  (fn [e]
                             (let [v (.-value (.-target e))]
                               (swap! state assoc :temp-value v)))
-              :onKeyPress (fn [e] (enter-handler e active-cell))}]])
+              :onKeyPress (fn [e] (press-enter-handler state e active-cell))}]])
          [:h5 "About"]
          [:p "This is an Excel-like spreadsheet with formulas, propagation, etc. Here are some things to try:"]
          [:ul
